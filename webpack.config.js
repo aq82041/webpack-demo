038ce7ba3d5a9f0d6d1ca02920cf782960e5368c
anqi <18742024663@163.com>
2020-04-23 20:14:30 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,15 @@
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 var path = require("path");
 
+const base = require("./webpack.config.base.js");
+
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  ...base,
   devtool: "inline-source-map",
   devServer: {
     contentBase: "./dist",
   },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[contenthash].js",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "demo",
-      template: "src/assets/index.html",
-    }),
-  ],
+  mode: "development",
   module: {
     rules: [
       {
