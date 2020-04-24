27efbe2c087e0a828b15f446fee6d159d1810541
anqi <18742024663@163.com>
2020-04-24 10:43:55 +0800

    准备上传

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import x from "./x.js";
 
-console.log(x);
-console.log("hi");
+import png from "./assets/1.png";
+const div = document.getElementById("app");
+div.innerHTML = `<img src='${png}'>`;
+
+const button = document.createElement("button");
+button.innerText = "懒加载";
+div.appendChild(button);
+button.onclick = () => {
+  const promise = import("./lazy.js"); //异步
+  promise.then(
+    (module) => {
+      const fn = module.default;
+      fn();
+    },
+    () => {
+      console.log("模块加载错误");
+    }
+  );
+};
